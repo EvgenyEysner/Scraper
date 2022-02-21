cae202c1b1db1f68f49c897f479c57d2b20add02
evgeny <evgeny.eysner@gmail.com>
2022-02-21 21:33:23 +0100

    Initial commit

---
+++ cian/scraper.py
@@ -0,0 +1,149 @@
+import random
+import requests
+import json
+import urllib.request
+from threading import Thread
+
+from django.http import JsonResponse
+from fake_useragent import UserAgent
+import time
+import asyncio
+import aiohttp
+from asgiref.sync import sync_to_async
+
+from django.core.files import File
+
+from .models import Apartment, Image, Task
+
+ua = UserAgent()
+
+headers = {
+    'User-Agent': ua.random,
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'
+}
+
+start_json_template = "window._cianConfig['frontend-offer-card'] = "
+
+# proxy = {
+#
+#      'https': 'http://186.65.114.25:9898',
+#      'http': 'http://186.65.114.25:9898'
+# }
+
+
+def get_page_data(url):
+
+    # qs = Task.objects.filter(user=user)
+    # for url in qs:
+    with requests.get(url, headers=headers, timeout=random.randint(1, 5)) as response:
+        html = response.text
+        if start_json_template in html:  # get json from website
+            start = html.index(start_json_template) + len(start_json_template)
+            end = html.index('</script>', start)
+            json_raw = html[start:end].strip()[:-1]
+            js = json.loads(json_raw)
+
+            for item in js:
+                apartments = []
+                if item['key'] == 'defaultState':
+                    try:
+                        price = item['value']['offerData']['offer']['bargainTerms']['price']
+                    except:
+                        price = None
+                    try:
+                        floor = item['value']['offerData']['offer']['floorNumber']
+                    except:
+                        floor = None
+                    try:
+                        desc = item['value']['offerData']['offer']['description']
+                    except:
+                        desc = None
+                    try:
+                        region = item['value']['offerData']['offer']['geo']['address'][0]['fullName']
+                    except:
+                        region = None
+                    try:
+                        town = item['value']['offerData']['offer']['geo']['address'][1]['fullName']
+                    except:
+                        town = None
+                    try:
+                        street = item['value']['offerData']['offer']['geo']['address'][-2]['fullName']
+                    except:
+                        street = None
+                    try:
+                        house_number = item['value']['offerData']['offer']['geo']['address'][-1]['fullName']
+                    except:
+                        house_number = None
+                    try:
+                        address = region + ', ' + town + ', ' + street + ' ' + house_number
+                        # address = item['value']['offerData']['offer']['geo']['address']
+                    except:
+                        address = None
+                    try:
+                        rooms = item['value']['offerData']['offer']['roomsCount']
+                    except:
+                        rooms = None
+                    try:
+                        commission = item['value']['offerData']['offer']['bargainTerms']['agentFee']
+                    except:
+                        commission = None
+                    try:
+                        photos = []
+
+                        for photo in item['value']['offerData']['offer']['photos']:
+                            photos.append(photo['fullUrl'])
+                    except:
+                        photos = None
+                    apartments.append(
+                        {
+                            'rooms': rooms,
+                            'price': price,
+                            'address': address,
+                            'desc': desc,
+                            'floor': floor,
+                            'photos': photos,
+                            'commission': commission,
+                        }
+                    )
+                    # save_data(apartments, user)
+                    return(apartments)
+
+
+def save_data(apartments_list, user):  # save the scraped data to the database
+    for ap in apartments_list:
+        try:
+            apartment = Apartment.objects.create(
+                owner=user,
+                rooms=ap['rooms'],
+                price=ap['price'],
+                address=ap['address'],
+                desc=ap['desc'],
+                floor=ap['floor'],
+                commission=ap['commission'],
+            )
+            for image in ap['photos']:
+                im = Image()
+                pic = urllib.request.urlretrieve(image)[0] # download images
+                im.img.save(image, File(open(pic, 'rb'))) # save images to media directory
+                im.apartment_id = apartment.pk
+                im.save()
+
+        except Exception as e:
+            print(e)
+            break
+
+# async def gather_data():
+#     async with aiohttp.ClientSession() as session:
+#         tasks = []
+#         apartments_list = asyncio.create_task(get_page_data(session))
+#         # task = asyncio.create_task(save_data(apartments_list))
+#         tasks.append(apartments_list)
+#         # tasks.append(task)
+#     await asyncio.gather(*tasks)
+#
+#
+# def main():
+#     asyncio.run(gather_data())
+
+
+
---
+++ cian/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import IndexView, ApartmentDeleteView, ApartmentDetailView, \
+    ApartmentUpdateView, ImageDeleteView, ImageUpdateView, apartments_render_pdf_view, ProfileUpdateView, UserProfile #get_page_data
+
+app_name = 'apartments'
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='home'),
+    path('apartment/<int:pk>/delete', ApartmentDeleteView.as_view(), name='delete'),
+    path('image/<int:pk>/delete', ImageDeleteView.as_view(), name='delete-image'),
+    path('apartment/<int:pk>/details', ApartmentDetailView.as_view(), name='apartment'),
+    path('apartment/<int:pk>/update', ApartmentUpdateView.as_view(), name='update'),
+    path('image/<int:pk>/update', ImageUpdateView.as_view(), name='update-image'),
+    path('apartment/<int:pk>/create_pdf', apartments_render_pdf_view, name='create_pdf'),
+    path('profile/', UserProfile.as_view(), name='profile'),
+    path('<int:pk>/edit', ProfileUpdateView.as_view(), name='edit'),
+    ]
